dc1309826784cabf6b5ad77c16e782afdbf9c915
Victor Henrique <victorhrvs@gmail.com>
2022-07-25 18:52:10 -0300

    Scroll between sections

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
-import logo from "./assets/Logo.png";
+import Header from "./components/Header.js";
 import "./App.css";
+import foto from "./assets/foto.png";
+import fotoBG from "./assets/fotoBG.png";
 
 function App() {
   return (
     <div className="App">
-      <section>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="Logo da Coopers" />
-          <nav>
-            <button>test</button>
-          </nav>
-        </header>
-      </section>
+      <div className="scroll-container">
+        <section className="scroll-area">
+          <Header />
+          <div className="Columns">
+            <div className="ColumnLeft">
+              <h1>
+                <strong>Organize</strong> your daily jobs
+              </h1>
+            </div>
+
+            <div className="ColumnRight">
+              <img id="foto" src={foto} alt="" />
+              <img id="fotoBG" src={fotoBG} alt="" />
+            </div>
+          </div>
+        </section>
+        <section className="scroll-area" >
+          <div className="Columns">
+            <div className="ColumnLeft">
+              <h1>
+                <strong>Organize</strong> your daily jobs
+              </h1>
+            </div>
+
+            <div className="ColumnRight">
+              <img id="foto" src={foto} alt="" />
+              <img id="fotoBG" src={fotoBG} alt="" />
+            </div>
+          </div>
+        </section>
+        <section className="scroll-area" >
+          <div className="Columns">
+            <div className="ColumnLeft">
+              <h1>
+                <strong>Organize</strong> your daily jobs
+              </h1>
+            </div>
+
+            <div className="ColumnRight">
+              <img id="foto" src={foto} alt="" />
+              <img id="fotoBG" src={fotoBG} alt="" />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import logo from "../assets/Logo.png";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="App-header">
+        <img src={logo} className="App-logo" alt="Logo da Coopers" />
+        <nav>
+        <button>entrar</button>
+        </nav>
+    </header>
+  );
+}
+
+export default Header;
