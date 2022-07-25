ee61a3f13779ace49dcf752c12b6c6e567e6ac23
Victor Henrique <victorhrvs@gmail.com>
2022-07-24 21:17:11 -0300

    header created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/Logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="Logo da Coopers" />
+          <nav>
+            <button>test</button>
+          </nav>
+        </header>
+      </section>
     </div>
   );
 }
