bd5504dd0d25c6d58240883b0bdb44b00a425a19
Victor Henrique <victorhrvs@gmail.com>
2022-07-28 06:49:44 -0300

    improvement of sections

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import Header from "./components/Header.js";
 import "./App.css";
 import foto from "./assets/foto.png";
 import fotoBG from "./assets/fotoBG.png";
-//import leftBG from "./assets/leftBG.png";
+import vector from "./assets/Vector.png";
+import image from "./assets/image.png";
+import iconmail from "./assets/icon-mail.png";
 
 function App() {
   return (
@@ -30,65 +32,94 @@ function App() {
           </div>
         </section>
 
-
-
-        <section  id="ToDoScreen">
+        <section id="ToDoScreen">
           <div id="title">
-          <div id="BG2Limit">
-            <div id="BG2"></div>
-          </div>
-          <h2>To-do List</h2>
-          <div id="line1"></div>
-          <h3 id="subtitle">Drag and drop to set your main priorities, check when done and create what´s new.</h3>
+            <div id="BG2Limit">
+              <div id="BG2"></div>
+            </div>
+            <h2>To-do List</h2>
+            <div id="line1"></div>
+            <h3 id="subtitle">
+              Drag and drop to set your main priorities, check when done and
+              create what´s new.
+            </h3>
           </div>
 
-
+          
           <div className="ToDos">
             <div className="ToDoLeft">
               <div>
+                <h3>To-do</h3>
+                <h4>Take a breath.</h4>
+                <h4>Start doing.</h4>
+                <div className="space"/>
 
+                <div id="ToDoContent">
+                  <img src={vector}  alt=""/>
+                  <p>ToDoContent</p>
+                  <a>delete</a>
+                </div>
+
+              <button>erase all</button>
               </div>
             </div>
 
             <div className="ToDoRight">
-              <div></div>
+              <div>
+                <h3>Done</h3>
+                <h4>Congratulions!</h4>
+                <h4>
+                  <strong>You have done 5 tasks</strong>
+                </h4>
+                <div className="space"/>
+                <div id="ToDoContent">
+                  <img src={vector}  alt=""/>
+                  <p>ToDoContent</p>
+                  <a>delete</a>
+                </div>
+                <div id="ToDoContent">
+                  <img src={vector}  alt=""/>
+                  <p>ToDoContent</p>
+                  <a>delete</a>
+                </div>
+                <button>erase all</button>
+              </div>
             </div>
           </div>
         </section>
-          
-        
-        <section className="scroll-area" id="carousel">
-          <div className="Columns">
-            <div className="ColumnLeft">
-              <h1>
-                <strong>Organize</strong> your daily jobs
-              </h1>
-            </div>
 
-            <div className="ColumnRight">
-              <img id="foto" src={foto} alt="" />
-              <img id="fotoBG" src={fotoBG} alt="" />
-            </div>
+        <section className="scroll-area" id="carousel">
+          <h2>good things</h2>
+          <div className="Carousel">
+            
           </div>
         </section>
 
         <section className="scroll-area" id="GetInTouch">
-          <h2>GET IN TOUCH</h2>
+          <img src={image}  alt=""/>
+          <div id="side"/>
+          <div className="GetInTouch">
+            <div className="iconGetIn">
+              <img src={iconmail}  alt=""/>
+              <div id="titleGetIn">
+                <h3>GET IN </h3>
+                <h3><strong>TOUCH</strong></h3>
+              </div>
+            </div>
+            <div>
+              oioioi
+            </div>
+
+          </div>
         </section>
-        
+
         <div id="lineBG"></div>
-        <footer  className="scroll-area" >
-          
-              
-            
-          
+        <footer className="scroll-area">
           <h3>Need help?</h3>
           <h4>coopers@coopers.pro</h4>
           <h5>© 2021 Coopers. All rights reserved.</h5>
-
+          <div id="end"/>
         </footer>
-        
-
       </div>
     </div>
   );
